d7fdd8a57d30c88b39ecee1c98fb1c1d51ba1b9e
DANIEL ALONSO SANCHEZ <DALOSA01@caser.local>
2017-12-04 15:40:19 +0100

    testeo como un controlador spring

--- src/test/java/es/caser/spring/mvc/controller/HomeControllerTest.java
+++ src/test/java/es/caser/spring/mvc/controller/HomeControllerTest.java
@@ -1,14 +1,16 @@
 package es.caser.spring.mvc.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import org.junit.Test;
-import org.springframework.ui.ExtendedModelMap;
-
+import org.springframework.test.web.servlet.MockMvc;
 public class HomeControllerTest {
 	@Test
 	public void testHomePage() throws Exception {
 		HomeController controller = new HomeController();
-		assertEquals("index", controller.sayHello(new ExtendedModelMap()));
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/")).andExpect(view().name("index"));
 	}
 }
